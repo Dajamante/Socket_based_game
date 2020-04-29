c4cd66987897c649852b39d5a3ca20a6e4595d4d
dajamante <aimaiga2@gmail.com>
2020-04-29 16:05:12 +0200

    basic multiclient

--- client.py
+++ client.py
@@ -1,39 +1,28 @@
 import socket
-import time 
+import time
 
 SERVER = "localhost"
 PORT = 65432
 
-class Network:
+
+class Client:
     def __init__(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((SERVER, PORT))
         self.addr = (SERVER, PORT)
 
     def run(self):
-        
-        msg = "string to revert"
+
+        msg = "String sent from client"
 
         while True:
-            time.sleep(0.5)
-            self.s.send(msg.encode('ascii')) 
-            
-            #Receive back string from server
-            #data = self.s.recv(1024) 
-            #print('Received from the server :',str(data.decode('ascii'))) 
-            
-            # print the received message 
-            # here it would be a reverse of sent message 
-            #print('Received from the server :',str(data.decode('ascii'))) 
-            
-            # # ask the client whether he wants to continue 
-            # ans = input('\nDo you want to continue(y/n) :') 
-            # if ans == 'y': 
-            #     continue
-            # else: 
-            #     break
-        # close the connection 
+            time.sleep(1)
+            self.s.send(msg.encode('ascii'))
+
+            retour = self.s.recv(1024)
+            print(retour)
         self.s.close()
 
-n = Network()
-n.run()
+
+c = Client()
+c.run()
--- game.py
+++ game.py
@@ -1,24 +1,23 @@
 from server import *
 import queue
+from _thread import *
+import threading
 
 
 def run_game():
-    #initate game
-    #initiate queue
+    # initate game
+    # initiate queue
     q = queue.Queue()
-    #start the server
+    # start the server
     # creates the server
     server = Server(q)
-    # launching the server and create threads for connections
-    server.run()
-    
+    # launching a threaded server and create threads for connections
+    start_new_thread(server.run, ())
+
     while True:
-        
         if not q.empty():
             i = q.get()
             print(i)
 
-     
 
 run_game()
-
